29d7d78335085904fa9ebbce6e6fe2abb4813588
Huseyin Koclar <75777777+huseyinkoclar@users.noreply.github.com>
2021-06-16 15:49:18 +0300

    Add files via upload

--- XSort.c
+++ XSort.c
@@ -14,16 +14,15 @@ char surname[15];
 char email[28];
 };
 
-struct header
-{
-int id;
-int grade;
-char name[15];
-char surname[15];
-char email[26];
-};
+typedef struct record rec;
 
 int main();
+
+struct record *readLine(char path[], int spesificLine, int size);
+
+void heapSort(struct record* arr[], int size);
+
+void heapify(struct record* arr[], int n, int i);
 
 const char* getfield(char* line, int num)
 {
@@ -54,49 +53,39 @@ int main()
     }
     printf("satir = %d\n",k+1);
     stream = fopen("students.csv", "r");
-    
-    char line[k];
-    int count = 0;
-    while(fgets(line, k, stream)){
-        char* tmp = strdup(line);
-        char* tmp2 = strdup(line);
-        char* tmp3 = strdup(line);
-        char* tmp4 = strdup(line); 
-        char* tmp5 = strdup(line);       
-        record.id =  atoi(getfield(tmp,1));
-        record.grade =  atoi(getfield(tmp5, 5));
-        strcpy(record.name, getfield(tmp2, 2));
-        strcpy(record.email, getfield(tmp4, 4));
-        strcpy(record.surname, getfield(tmp3, 3));
-        
-        count++;
-        free(tmp);
-        free(tmp2);
-        free(tmp3);
-        free(tmp4);
-        free(tmp5);
+  
+    //printf(readLine("students.csv", 1, k).name);
+    //replacementselectionSort("students.csv", "output.csv", k, 4);
+    struct record* arr[5];
+    for(int i = 0; i<6;i++){
+      arr[i] = readLine("students.csv", i+1, k);
     }
-    printf("%d   ",record.id);
-    printf(record.name);
-    printf("  ");
-    printf(record.surname);
-    printf("   ");
-    printf(record.email);
-    printf("   ");
-    printf("%d", record.grade);
+    printf("%d \n", sizeof(arr));
+    heapSort(arr,(sizeof arr/sizeof arr[0])+1);
+    int a = sizeof arr/sizeof arr[0];
     
+    printf("%d\n",arr[0]->id);
+    printf("%d\n",arr[1]->id);
+    printf("%d\n",arr[2]->id);
+    printf("%d\n",arr[3]->id);
+    printf("%d\n",arr[4]->id);
+    printf("%d\n",arr[5]->id);
+
+    struct record* deneme =readLine("students.csv", 5, k);
+    printf(deneme->name);
+    printf("basarili");
 }
 
-void replacementselectionSort(char path[], char path2[], char outpath[], int pageSize, int bufferCount)
+/*
+void replacementselectionSort(char path[], char outpath[], int pageSize, int bufferCount)
 {
     struct record record;
     int recordSize;
     int recordCount = pageSize/recordSize;
-    
+    struct record heap[4];
     FILE* stream;
     stream = fopen(path, "r");
     struct record in[recordCount];
-    bool endOfFile = ReadLines(path, recordCount);
     struct record out[recordCount * sizeof(record)];
     heapSort(recordCount,in);
     struct record list[recordCount];
@@ -104,28 +93,26 @@ void replacementselectionSort(char path[], char path2[], char outpath[], int pag
     int i = 0;
     int j = 0;
     int k = 0;
-
-    
+    int c = 4;
     while(i != pageSize){
         while(!heapEmpty(heap)){
 
-            struct record rec = heap.pop();
+            struct record rec = heap[(sizeof(heap)/sizeof(heap[0]) - 1)];
             lastEject = rec.id;
             out[i] = rec;
-
-            struct record newRec = readLines(stream,1);
-
+            
+            struct record newRec = *readLine(path, c, pageSize);
+            c++;
             if(newRec.id<lastEject){
                 list[j] = newRec;
                 j++;
             }
             else{
-                heap.push(newRec);
+                heap[sizeof(heap)/sizeof(heap[0]) - 1] = newRec;
             }
         }
         free(heap);
-        heap = createHeap(recordCount, list);
-        FILE *fpt = fopen(path2,"w+");
+        FILE *fpt = fopen(outpath,"w+");
         fprintf(fpt, out);
         fclose(fpt);
         free(fpt);
@@ -133,45 +120,72 @@ void replacementselectionSort(char path[], char path2[], char outpath[], int pag
     }
     
 }
+*/
+struct record* temp;  
+  
+void heapify(struct record* arr[], int size, int i)  
+{  
+int largest = i;    
+int left = 2*i + 1;    
+int right = 2*i + 2;    
+  
+if (left < size && arr[left]->id >arr[largest]->id)  
+largest = left;  
+  
+if (right < size && arr[right]->id > arr[largest]->id)  
+largest = right;  
+  
+if (largest != i)  
+{  
+temp = arr[i];  
+    arr[i]= arr[largest];   
+    arr[largest] = temp;  
+heapify(arr, size, largest);  
+}  
+}  
+  
+void heapSort(struct record* arr[], int size) {  
 
-
- // Function to swap the the position of two elements
-  void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+for (int i = size / 2 - 1; i >= 0; i--)  {
+  heapify(arr, size, i);  
+}
+for (int i=size-1; i>=0; i--)  
+{  
+    temp = arr[0];  
+    arr[0]= arr[i];   
+    arr[i]= temp;  
+    heapify(arr, i, 0);  
+}  
+}  
+    
+  struct record* readLine(char path[], int spesificLine, int size){
+    struct record* record = calloc(51, sizeof(struct record));
+    FILE *stream = fopen(path, "r");
+    
+    char line[size];
+    int count = 0;
+    while(fgets(line, size, stream)){
+      if(count==spesificLine){
+        char* tmp = strdup(line);
+        char* tmp2 = strdup(line);
+        char* tmp3 = strdup(line);
+        char* tmp4 = strdup(line); 
+        char* tmp5 = strdup(line);       
+        record->id =  atoi(getfield(tmp,1));
+        record->grade =  atoi(getfield(tmp5, 5));
+        strcpy(record->name, getfield(tmp2, 2));
+        strcpy(record->email, getfield(tmp4, 4));
+        strcpy(record->surname, getfield(tmp3, 3));
+        free(tmp);
+        free(tmp2);
+        free(tmp3);
+        free(tmp4);
+        free(tmp5);
+        break;
+      }
+      count++;
+    }
+    return record;
   }
   
-  void heapify(int arr[], int n, int i) {
-    // Find largest among root, left child and right child
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
   
-    if (left < n && arr[left] > arr[largest])
-      largest = left;
-  
-    if (right < n && arr[right] > arr[largest])
-      largest = right;
-  
-    // Swap and continue heapifying if root is not largest
-    if (largest != i) {
-      swap(&arr[i], &arr[largest]);
-      heapify(arr, n, largest);
-    }
-  }
-  
-  // Main function to do heap sort
-  void heapSort(int arr[], int n) {
-    // Build max heap
-    for (int i = n / 2 - 1; i >= 0; i--)
-      heapify(arr, n, i);
-  
-    // Heap sort
-    for (int i = n - 1; i >= 0; i--) {
-      swap(&arr[0], &arr[i]);
-  
-      // Heapify root element to get highest element at root again
-      heapify(arr, i, 0);
-    }
-  } 
